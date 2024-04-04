f9ff4ad098728ab4c8d8189cc83b0acfe2914daf
PALANCA Marc <marc.ramos@ext.soprasteria.com>
2024-04-04 11:54:25 +0200

    Ejercicio 3

--- src/app/components/aside/aside.component.ts
+++ src/app/components/aside/aside.component.ts
@@ -9,26 +9,14 @@ import { Product } from 'src/app/interface/Product';
 export class AsideComponent {
   @Input() products: Product[] = {} as Product[];
   @Input() productSelected: Product = {} as Product;
-  @Input() filterStyles: String[] = {} as String[];
+  @Input() activeFilter: String = {} as String;
   @Output() onChangeProduct: EventEmitter<Product> = new EventEmitter();
-  @Output() onFilterProductsLessPrice: EventEmitter<void> = new EventEmitter();
-  @Output() onFilterProductsMorePrice: EventEmitter<void> = new EventEmitter();
-  @Output() onFilterProductsTopRating: EventEmitter<void> = new EventEmitter();
-  @Output() onResetFilters: EventEmitter<void> = new EventEmitter();
+  @Output() onChangeFilter: EventEmitter<string> = new EventEmitter();
   changeProduct(productChange: Product) {
     this.onChangeProduct.emit(productChange);
   }
 
-  filterProductsLessPrice() {
-    this.onFilterProductsLessPrice.emit();
-  }
-  filterProductsMorePrice() {
-    this.onFilterProductsMorePrice.emit();
-  }
-  filterProductsTopRating() {
-    this.onFilterProductsTopRating.emit();
-  }
-  resetFilters() {
-    this.onResetFilters.emit();
+  changeFilter(filter: string) {
+    this.onChangeFilter.emit(filter);
   }
 }
